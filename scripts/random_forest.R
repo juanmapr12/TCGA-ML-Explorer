2592987f23d91a7fcdf3e45e096bff803d2893b0
Juan Manuel Pérez Rogerio <juaperrog@alum.us.es>
2025-07-22 20:32:10 +0200

    Segundo cambio

--- scripts/random_forest.R
+++ scripts/random_forest.R
@@ -7,9 +7,9 @@ library(survival)
 library(caret)
 
 # Separamos en entrenamiento y prueba:
-trainIndex <- createDataPartition(new_clinical_df$vital_status, p = 0.8, list = FALSE)
-train_data <- new_clinical_df[trainIndex, ]
-test_data <- new_clinical_df[-trainIndex, ]
+trainIndex <- createDataPartition(clinical_df$vital_status, p = 0.8, list = FALSE)
+train_data <- clinical_df[trainIndex, ]
+test_data <- clinical_df[-trainIndex, ]
 
 
 # Modelo Random Forest
@@ -24,16 +24,3 @@ confusionMatrix(rf_preds, test_data$vital_status)
 
 # Importancia de variables
 varImpPlot(modelo_rf)
-
-
-
-# Modelo Random Survival Forest
-
-# #train_data_2 <- train_data %>% select(-time)
-# rsf_model <- rfsrc(Surv(vital_status,time) ~ ., data = train_data, ntree = 1000, nodesize = 5, nsplit = 50, importance = TRUE)
-# plot(rsf_model)
-# print(rsf_model)
-
-# RALLADA MENTAL. Aunque creo que nos puede venir bien para
-# únicamente los miRNAs, no sé. Pero no todos obvio, quizás los que
-# sobrevivan del análisis de supervivencia.
